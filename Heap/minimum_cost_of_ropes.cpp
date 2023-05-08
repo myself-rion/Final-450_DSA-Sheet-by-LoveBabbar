0b245679c41bcc0e6b62859ba5f35097793d64ab
Rion Talukder <94348928+myself-rion@users.noreply.github.com>
2023-05-08 21:06:17 +0530

    minimum cost of ropes using min heap

--- Heap/minimum_cost_of_ropes.cpp
+++ Heap/minimum_cost_of_ropes.cpp
@@ -1,7 +1,7 @@
 long long minCost(long long arr[], long long n)
 {
 
-    priority_queue<long long, vector<long long>, greater<long long>> minHeap;
+    priority_queue<long long, vector<long long>, greater<long long>> minHeap; //creating min Heap because top of min heap is the smallest element
     long long cost = 0;
 
     for (long long i = 0; i < n; i++)
@@ -11,6 +11,7 @@ long long minCost(long long arr[], long long n)
 
     while (minHeap.size() > 1)
     {
+        //popping top 2 minimum elements
         long long a = minHeap.top();
         minHeap.pop();
 
@@ -18,7 +19,8 @@ long long minCost(long long arr[], long long n)
         minHeap.pop();
 
         cost += a + b;
-
+        
+        // pushing the sum in heap
         minHeap.push(a + b);
     }
 
